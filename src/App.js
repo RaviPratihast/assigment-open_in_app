3d3baecc6187ce8c3a917ba977c606ffe6d1058f
RaviPratihast <ravipratihast@gmail.com>
2023-09-01 23:52:04 +0530

    test: test deployment.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header bg-red-200">
-        Assingment
+       Project Setup!!!
       </header>
     </div>
   );
