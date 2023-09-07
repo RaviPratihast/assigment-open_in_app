42fc3cc4642a070edd3b9d520d9825d090fa6313
RaviPratihast <ravipratihast@gmail.com>
2023-09-07 07:55:32 +0530

    docs: changes in docs, by uploading data for chart.

---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+const Data = [
+  { id: 1, week: "Week1", user: 400, guest: 500 },
+  { id: 2, week: "Week2", user: 100, guest: 200 },
+  { id: 3, week: "Week3", user: 200, guest: 300 },
+  { id: 4, week: "Week4", user: 500, guest: 100 },
+];
+
+export { Data };
